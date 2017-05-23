46f14a2c3d96cf38eb794c3086aac6cf55750dcf
mahynski <nathan.mahynski@gmail.com>
2017-05-23 00:21:42 -0400

    added raw for binary fs lj

---
+++ binary_fslj/raw/1/run.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append("../../py/")
+import binary_fslj as bfslj
+
+if __name__ == "__main__":
+	x = bfslj.binary_fslj()
+	x.run(steps=int(1e7), orderMin=0, orderMax=235, temp=1.50, mu1=0.0, dmu2=1.65, order=3)
---
+++ binary_fslj/raw/2/run.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append("../../py/")
+import binary_fslj as bfslj
+
+if __name__ == "__main__":
+	x = bfslj.binary_fslj()
+	x.run(steps=int(1e7), orderMin=229, orderMax=325, temp=1.50, mu1=0.0, dmu2=1.65, order=3)
